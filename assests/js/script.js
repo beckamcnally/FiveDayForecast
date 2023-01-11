57a9967df028aa057a195d7667e94dfa8d3d410d
Becka McNally <beckamcnally@gmail.com>
2023-01-11 09:15:54 -0800

    forecast cards

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,8 @@
 var userCityField = document.getElementById('userCity')
 var getWeather = document.getElementById('getWeather')
 var btnContainer = document.getElementById('btnContainer')
+var form = document.getElementById('form')
+var wContainer = document.getElementById('wContainer')
 var usercity;
 var cities = [];
 
@@ -9,9 +11,9 @@ var cities = [];
 
 // log city searched and generate 
 function processClick (event) {
-    
     event.preventDefault()
     usercity = userCityField.value
+    userCityField.textContent = "" // didnt work
     cities.push(usercity)
    
     var citiesStringify = JSON.stringify(cities)
@@ -22,11 +24,16 @@ function processClick (event) {
     
 // the users choice will go into a list of buttons that make up the users previously searched cities
 // send to local storage first and then take for loop the array that comes out of local storage
+    
+while (btnContainer.firstChild) {
+    btnContainer.removeChild(btnContainer.firstChild);
+}
+
 for (var i = 0; i < citiesParsed.length; i++) {
-        var city = citiesParsed[i]
-        var liEl = document.createElement("li")
-        var btnEl = document.createElement("button")
-        btnContainer.appendChild(liEl) 
+    var city = citiesParsed[i]
+    var liEl = document.createElement("li")
+    var btnEl = document.createElement("button")
+    btnContainer.appendChild(liEl) 
         liEl.appendChild(btnEl)
         btnEl.textContent = city
     }      
@@ -36,7 +43,7 @@ for (var i = 0; i < citiesParsed.length; i++) {
 // when the user types in the city they are looking for the input box will shift and the page will then be shared with the requested information
      
     // A: need the browser to listen for the click so that it can run two functions one for saving the city names and dynamically creating the list of previously searched cities and the other to run a series of functions like the request for the api search and translation of data retrieval. 
-getWeather.addEventListener("click", processClick)
+    form.addEventListener("submit", processClick)
 // series of functions that request and process that data for the city provide
 // weather data can be obtained in JSON
      // Step 
@@ -49,21 +56,76 @@ getWeather.addEventListener("click", processClick)
     
 // function that fetches the api information
 function callWeather() {
-    var api = "https://api.openweathermap.org/data/2.5/forecast?q=" + usercity + "&appid=831d578d1e311567a319b8a0576c71e4"
+  var api = "https://api.openweathermap.org/data/2.5/forecast?q=" + usercity + "&appid=831d578d1e311567a319b8a0576c71e4&units=imperial"
 
- fetch(api)
+  fetch(api)
     
     .then(function (response) {
         return response.json();
       })
     .then(function (data) {
-        console.log(data);
+        console.log(data)
+        for (var i = 6; i < data.list.length; i+=8) {
+            var cardDiv = document.createElement("div")
+            cardDiv.setAttribute("id", "cardDiv")
+            var cardHead = document.createElement("h2")
+            var iconCode = data.list[i].weather[0].icon
+            var icon = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+            var cardIcon = document.createElement("i")
+            cardIcon.setAttribute("src", icon)
+            var temp = document.createElement("p")
+            var wind = document.createElement("p")
+            var humidity = document.createElement("p")
+
+            cardHead.textContent = data.list[i].dt_txt
+            temp.textContent = data.list[i].main.temp
+            wind.textContent = data.list[i].wind.speed
+            console.log(humidity.textContent = data.list[i].main.humidity)
+            
+
+            wContainer.append(cardDiv)
+            cardDiv.append(cardHead)
+            cardDiv.append(cardIcon)
+            cardDiv.append(temp)
+            cardDiv.append(wind)
+            cardDiv.append(humidity)
+
+console.log("date " + data.list[i].dt_txt);
+        
+        
+         console.log(data.list[i].weather[0].icon); 
+        // console.log("temp " + data.list[0].main.temp);
+        // console.log("wind " + data.list[0].wind.speed);
+        // console.log("humidity " + data.list[0].main.humidity);
+    }
+         
    });  
 
+
+  var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + usercity + "&appid=831d578d1e311567a319b8a0576c71e4&units=imperial"
+
+  fetch(apiCurrent)
+    
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log("whatever")
+        var curDiv = document.createElement("div")
+        var curHead = document.createElement("h2")
+        var curIcon = document.createElement("i")
+        var curUl = document.createElement("ul")
+        var curLi = document.createElement("li")
+        // console.log(data)
+        // console.log("name " + data.name);
+        // console.log("date " + data.dt_txt); // ?? need to check this one
+        // // console.log(data.weather[0]).icon; does not like the icon for some reason
+        // console.log("temp " + data.main.temp);
+        // console.log("wind " + data.wind.speed);
+        // console.log("humidity " + data.main.humidity);
+        
+   });  
 }
-
-
-
 
 // the requested information will be broken into two parts - the current days forecast and the next five day
      // Step 
