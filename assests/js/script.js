82a325e32e3e77e25c29380d4b79f3aaebb39784
Becka McNally <beckamcnally@gmail.com>
2023-01-10 16:45:09 -0800

    fetch call

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,40 +1,71 @@
-// 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+var userCityField = document.getElementById('userCity')
+var getWeather = document.getElementById('getWeather')
+var btnContainer = document.getElementById('btnContainer')
+var usercity;
+var cities = [];
 
 
+
+
+// log city searched and generate 
+function processClick (event) {
     
+    event.preventDefault()
+    usercity = userCityField.value
+    cities.push(usercity)
+   
+    var citiesStringify = JSON.stringify(cities)
+    localStorage.setItem("cities", citiesStringify)
 
-
-    // Step 
-    // Q: What are you going to do?
-    // A: use local storage to save the data
-    // Q: How are you going to do it?
-    // A:
-    // Q: Why are you going to do it?
-    // A:
-
-
-// user can choose for their list of previously searched cities to display.
-// when you open the page there will be a header, in the center of the page there will be a text box with a label of enter the city that you would like to see the weather for and a submit button
-// Step 
-    // Q: What are you going to do?
-    // A: collect the users input 
-    // Q: How are you going to do it?
-    // A: save it to a var (userInput)
-    // Q: Why are you going to do it?
-    // A: for future use with the city location and list of previously searched cities
+    var storedCities = localStorage.getItem("cities")
+    var citiesParsed = JSON.parse(storedCities)
     
+// the users choice will go into a list of buttons that make up the users previously searched cities
+// send to local storage first and then take for loop the array that comes out of local storage
+for (var i = 0; i < citiesParsed.length; i++) {
+        var city = citiesParsed[i]
+        var liEl = document.createElement("li")
+        var btnEl = document.createElement("button")
+        btnContainer.appendChild(liEl) 
+        liEl.appendChild(btnEl)
+        btnEl.textContent = city
+    }      
+// } 
+    callWeather(usercity)
+}
 // when the user types in the city they are looking for the input box will shift and the page will then be shared with the requested information
+     
+    // A: need the browser to listen for the click so that it can run two functions one for saving the city names and dynamically creating the list of previously searched cities and the other to run a series of functions like the request for the api search and translation of data retrieval. 
+getWeather.addEventListener("click", processClick)
+// series of functions that request and process that data for the city provide
+// weather data can be obtained in JSON
      // Step 
     // Q: What are you going to do?
-    // A: add an event listener for the submit button
+    // A: create a fetch request for the data
     // Q: How are you going to do it?
-    // A: using js
+    // A: using the built in fetch method
     // Q: Why are you going to do it?
-    // A: need the browser to listen for the click so that it can run two functions one for saving the city names and dynamically creating the list of previously searched cities and the other to run a series of functions like the request for the api search and translation of data retrieval. 
+    // A: so that I can retrieve the data from the OpenWeather API
+    
+// function that fetches the api information
+function callWeather() {
+    var api = "https://api.openweathermap.org/data/2.5/forecast?q=" + usercity + "&appid=831d578d1e311567a319b8a0576c71e4"
+
+ fetch(api)
+    
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log(data);
+   });  
+
+}
 
 
 
-// the requested information will be broken into two parts - the current days forcast and the next five day
+
+// the requested information will be broken into two parts - the current days forecast and the next five day
      // Step 
     // Q: What are you going to do?
     // A: dynamically generate a table 
@@ -53,11 +84,14 @@
     // A: so that i can see how the data is organized
 
 
-// the users choice will go into a list of buttons that make up the users previously searched cities
+
+    
+
     // Step 
     // Q: What are you going to do?
-    // A: dynamically create event listeners for the new buttons
+    // A: 
     // Q: How are you going to do it?
-    // A: if the for loop that creates the buttons i will add a dynamically created event listener
+    // A:
     // Q: Why are you going to do it?
-    // A: so that each time a button is clicked the series of functions for the search will begin
+    // A:
+
